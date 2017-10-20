64b2fc320c2d01d38dbc93f76155f3f41ae7e114
shalurai <shaluraics22@gmail.com>
2017-10-20 20:09:33 +0900

    getting data using async

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
 import escapeRegExp from 'escape-string-regexp'
-import * as DataApi from './Data'
+import * as DataApi from './api'
 
+// export async function fetchData() {
+//   let url = 'https://gist.githubusercontent.com/shalurai/942783a364a260c66d877a0ce957afc5/raw/4ac6ce02f2ab7c4087dc42b4d507b559bb21415b/Data.json';
+//   let response = await fetch(url);
+//  let body = await response.json();
+//  let bodyData = await body.Reggae;
+//   console.log("body", bodyData);
+//   return bodyData;
+//
+// }
 
 export default class App extends Component {
 
@@ -10,10 +19,9 @@ export default class App extends Component {
      data:[]
 }
 
-componentDidMount(){
-  console.log("DataApi",DataApi.Reggae);
-  const data = DataApi.Reggae;
-  this.setState({data:data})
+
+componentDidMount =() => {
+ DataApi.fetchData().then(data => this.setState({data}))
 }
 
 updateQuery = (query) =>{
---
+++ src/api.js
@@ -0,0 +1,10 @@
+
+export async function fetchData() {
+  let url = 'https://gist.githubusercontent.com/shalurai/942783a364a260c66d877a0ce957afc5/raw/4ac6ce02f2ab7c4087dc42b4d507b559bb21415b/Data.json';
+  let response = await fetch(url);
+ let body = await response.json();
+ let bodyData = await body.Reggae;
+  console.log("body", bodyData);
+  return bodyData;
+
+}
