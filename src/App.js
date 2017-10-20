e8000a58c76169ba5c7b32937393e5935b6aeeaf
shalurai <shaluraics22@gmail.com>
2017-10-20 20:14:57 +0900

    Used Async function on same page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import escapeRegExp from 'escape-string-regexp'
-import * as DataApi from './api'
+//import * as DataApi from './api'
 
-// export async function fetchData() {
-//   let url = 'https://gist.githubusercontent.com/shalurai/942783a364a260c66d877a0ce957afc5/raw/4ac6ce02f2ab7c4087dc42b4d507b559bb21415b/Data.json';
-//   let response = await fetch(url);
-//  let body = await response.json();
-//  let bodyData = await body.Reggae;
-//   console.log("body", bodyData);
-//   return bodyData;
-//
-// }
+export async function fetchData() {
+  let url = 'https://gist.githubusercontent.com/shalurai/942783a364a260c66d877a0ce957afc5/raw/4ac6ce02f2ab7c4087dc42b4d507b559bb21415b/Data.json';
+  let response = await fetch(url);
+ let body = await response.json();
+ let bodyData = await body.Reggae;
+  console.log("body", bodyData);
+  return bodyData;
+
+}
 
 export default class App extends Component {
 
@@ -21,7 +21,8 @@ export default class App extends Component {
 
 
 componentDidMount =() => {
- DataApi.fetchData().then(data => this.setState({data}))
+ //DataApi.fetchData().then(data => this.setState({data}))
+ fetchData().then(data => this.setState({data}))
 }
 
 updateQuery = (query) =>{
