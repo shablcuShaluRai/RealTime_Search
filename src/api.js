93f5ddb582a0dd712f405ef5930db63191e92020
shalurai <shaluraics22@gmail.com>
2017-10-20 20:45:27 +0900

    Added Readme.md

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
 
+
 export async function fetchData() {
   let url = 'https://gist.githubusercontent.com/shalurai/942783a364a260c66d877a0ce957afc5/raw/4ac6ce02f2ab7c4087dc42b4d507b559bb21415b/Data.json';
   let response = await fetch(url);
