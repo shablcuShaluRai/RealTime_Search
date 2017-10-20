dfcf514a34156a30b18bdddcf2f041332b2fe8e5
shalurai <shaluraics22@gmail.com>
2017-10-20 13:36:51 +0900

    show list of people

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+
 
 class App extends Component {
+
+   state = {
+     data:
+     {
+           "reggae": [
+           "The Abyssinians",
+           "The Aces",
+           "Glen Adams",
+           "Admiral T",
+           "Yasus Afari",
+           "African Brothers",
+           "The Aggrovators",
+           "Aisha",
+           "Bobby Aitken",
+           "Laurel Aitken",
+           "Alaine",
+           "Alborosie"
+         ]
+     }
+
+}
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>React search</h1>
+        <p>here is a list of people rendered from a json object </p>
+        {this.state.data.reggae.map(data => <li>{data}</li>
+        )}
+
       </div>
     );
   }
